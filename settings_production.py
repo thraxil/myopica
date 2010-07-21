6939a5f1a719c01d70975b016bc0ab6bd2137b23
Anders Pearson <anders@columbia.edu>
2010-07-21 11:25:06 -0400

    support local_settings overrides in production

--- settings_production.py
+++ settings_production.py
@@ -7,3 +7,9 @@ MEDIA_ROOT = '/var/www/myopica/media/'
 DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
+# local_settings.py can be used to override environment-specific settings
+# like database and email that differ between development and production.
+try:
+    from local_settings import *
+except ImportError:
+    pass
