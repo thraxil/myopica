fa9f1363d3617d57ac0664d0872db67b686ac634
anders <anders@columbia.edu>
2013-01-04 22:25:49 -0500

    flake8 cleanup

--- portfolio/models.py
+++ portfolio/models.py
@@ -2,23 +2,24 @@ from django.db import models
 from django.contrib import admin
 from django.template.defaultfilters import slugify
 
+
 class Image(models.Model):
     title = models.CharField(max_length=256)
-    slug = models.SlugField(max_length=256,editable=False)
+    slug = models.SlugField(max_length=256, editable=False)
     description = models.TextField(blank=True)
     created = models.DateTimeField(auto_now_add=True)
-    medium = models.CharField(max_length=256,blank=True)
-    ahash = models.CharField(max_length=256,default="",null=True)
-    extension = models.CharField(max_length=256,default=".jpg",null=True)
+    medium = models.CharField(max_length=256, blank=True)
+    ahash = models.CharField(max_length=256, default="", null=True)
+    extension = models.CharField(max_length=256, default=".jpg", null=True)
 
-    def save(self,*args,**kwargs):
+    def save(self, *args, **kwargs):
         slug = slugify(self.title)[:255]
         addto = 1
         while Image.objects.filter(slug=slug).count() > 0:
             slug = slug + str(addto)
             addto += 1
         self.slug = slug
-        super(Image, self).save(*args,**kwargs)
+        super(Image, self).save(*args, **kwargs)
 
     def __unicode__(self):
         return self.title
@@ -26,18 +27,20 @@ class Image(models.Model):
     def get_absolute_url(self):
         return "/image/%s/" % self.slug
 
-    def add_to_gallery(self,gallery):
-        if GalleryImage.objects.filter(image=self,gallery=gallery).count() > 0:
+    def add_to_gallery(self, gallery):
+        if GalleryImage.objects.filter(
+            image=self,
+            gallery=gallery).count() > 0:
             return
-        gi = GalleryImage.objects.create(image=self,gallery=gallery,
-                                         ordinality=gallery.galleryimage_set.all().count() + 1)
+        GalleryImage.objects.create(
+            image=self, gallery=gallery,
+            ordinality=gallery.galleryimage_set.all().count() + 1)
         gallery.update_ordinality()
 
-    def remove_from_gallery(self,gallery):
-        gi = GalleryImage.objects.filter(gallery=gallery,image=self)[0]
+    def remove_from_gallery(self, gallery):
+        gi = GalleryImage.objects.filter(gallery=gallery, image=self)[0]
         gi.delete()
         gallery.update_ordinality()
-
 
     def prev_image(self):
         r = Image.objects.filter(created__gt=self.created).order_by("created")
@@ -61,6 +64,7 @@ class ImageAdmin(admin.ModelAdmin):
 def count_galleries():
     return Gallery.objects.all().count()
 
+
 class Gallery(models.Model):
     title = models.CharField(max_length=256)
     slug = models.SlugField()
@@ -74,20 +78,26 @@ class Gallery(models.Model):
         return "/%s/" % self.slug
 
     def images(self):
-        return [i.image for i in self.galleryimage_set.all().order_by("-ordinality")]
+        return [i.image
+                for i
+                in self.galleryimage_set.all().order_by("-ordinality")]
 
     def all_images(self):
-        return [i.image for i in self.galleryimage_set.all().order_by("ordinality")]
+        return [i.image
+                for i
+                in self.galleryimage_set.all().order_by("ordinality")]
 
     def first_images(self):
-        return [i.image for i in self.galleryimage_set.all().order_by("-ordinality")][:15]
+        return [i.image
+                for i
+                in self.galleryimage_set.all().order_by("-ordinality")][:15]
 
     def first_image(self):
         return self.galleryimage_set.all().order_by("-ordinality")[0].image
 
-
     def newest_images(self):
-        return [i.image for i in self.galleryimage_set.all().order_by("-id")][:5]    
+        return [i.image
+                for i in self.galleryimage_set.all().order_by("-id")][:5]
 
     def update_ordinality(self):
         cnt = 1
@@ -96,7 +106,7 @@ class Gallery(models.Model):
             gi.save()
             cnt += 1
 
-    def has_image(self,image):
+    def has_image(self, image):
         gallery_ids = [gi.gallery.id for gi in image.galleryimage_set.all()]
         return self.id in gallery_ids
 
@@ -104,29 +114,40 @@ class Gallery(models.Model):
 def count_images():
     return Image.objects.all().count()
 
+
 class GalleryAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
-    list_display = ('title','ordinality')
+    list_display = ('title', 'ordinality')
+
 
 class GalleryImage(models.Model):
     gallery = models.ForeignKey(Gallery)
-    image   = models.ForeignKey(Image)
+    image = models.ForeignKey(Image)
     ordinality = models.PositiveSmallIntegerField(default=count_images)
 
     def __unicode__(self):
-        return u"%s [%02d] %s" % (self.gallery,self.ordinality,self.image)
+        return u"%s [%02d] %s" % (self.gallery, self.ordinality,
+                                  self.image)
 
     def prev_image(self):
-        r = GalleryImage.objects.filter(gallery=self.gallery,
-                                        ordinality__gte=self.ordinality).exclude(image=self.image).order_by("ordinality","image")
+        r = GalleryImage.objects.filter(
+            gallery=self.gallery,
+            ordinality__gte=self.ordinality
+            ).exclude(
+            image=self.image
+            ).order_by("ordinality", "image")
         if r.count():
             return r[0]
         else:
             return None
 
     def next_image(self):
-        r = GalleryImage.objects.filter(gallery=self.gallery,
-                                        ordinality__lte=self.ordinality).exclude(image=self.image).order_by("-ordinality","-image")
+        r = GalleryImage.objects.filter(
+            gallery=self.gallery,
+            ordinality__lte=self.ordinality
+            ).exclude(
+            image=self.image
+            ).order_by("-ordinality", "-image")
         if r.count():
             return r[0]
         else:
@@ -141,5 +162,6 @@ class GalleryImage(models.Model):
     def other_galleries(self):
         return self.image.galleryimage_set.all().exclude(gallery=self.gallery)
 
-class GalleryImageAdmin(admin.ModelAdmin):pass
 
+class GalleryImageAdmin(admin.ModelAdmin):
+    pass
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,8 +1,8 @@
-from django.http import HttpResponse,HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render_to_response
-from models import *
+from models import Gallery, Image, GalleryImage
 from django.contrib.auth.decorators import login_required
-from forms import *
+from forms import AddImageForm
 from django.template.defaultfilters import slugify
 import urllib2
 from poster.encode import multipart_encode, MultipartParam
@@ -14,59 +14,76 @@ import os
 def index(request):
     images = Image.objects.all().order_by("-created")[:15]
     galleries = Gallery.objects.all().order_by("ordinality")
-    return render_to_response("index.html",dict(images=images,galleries=galleries))
+    return render_to_response("index.html",
+                              dict(images=images, galleries=galleries))
+
 
 def stream(request):
-    return render_to_response("stream.html",dict(images=Image.objects.all().order_by("-id")[:15]))
+    return render_to_response(
+        "stream.html",
+        dict(images=Image.objects.all().order_by("-id")[:15]))
 
-def scroll(request,id):
-    return render_to_response("scroll.html",dict(images=Image.objects.filter(id__lt=id).order_by("-id")[:15]))
 
-def gallery(request,slug):
-    gallery = get_object_or_404(Gallery,slug=slug)
-    return render_to_response("gallery.html",dict(gallery=gallery))
+def scroll(request, id):
+    return render_to_response(
+        "scroll.html",
+        dict(images=Image.objects.filter(id__lt=id).order_by("-id")[:15]))
 
-def reorder_gallery(request,slug):
-    gallery = get_object_or_404(Gallery,slug=slug)
+
+def gallery(request, slug):
+    gallery = get_object_or_404(Gallery, slug=slug)
+    return render_to_response("gallery.html",
+                              dict(gallery=gallery))
+
+
+def reorder_gallery(request, slug):
+    gallery = get_object_or_404(Gallery, slug=slug)
     if request.method == "GET":
-        return render_to_response("reorder_gallery.html",dict(gallery=gallery))
+        return render_to_response("reorder_gallery.html",
+                                  dict(gallery=gallery))
     else:
         for k in request.POST.keys():
             if not k.startswith('image-'):
                 continue
             ordinality = int(request.POST[k])
-            image = get_object_or_404(Image,id=k[len('image-'):])
-            gi = get_object_or_404(GalleryImage,gallery=gallery,image=image)
+            image = get_object_or_404(Image, id=k[len('image-'):])
+            gi = get_object_or_404(GalleryImage, gallery=gallery, image=image)
             gi.ordinality = ordinality
             gi.save()
         return HttpResponse("POST")
 
 
-def gallery_image(request,gallery_slug,image_slug):
-    gallery = get_object_or_404(Gallery,slug=gallery_slug)
-    image = get_object_or_404(Image,slug=image_slug)
-    gi = get_object_or_404(GalleryImage,gallery=gallery,image=image)
-    return render_to_response("gallery_image.html",dict(gallery=gallery,
-                                                        image=image,
-                                                        gi=gi))
+def gallery_image(request, gallery_slug, image_slug):
+    gallery = get_object_or_404(Gallery, slug=gallery_slug)
+    image = get_object_or_404(Image, slug=image_slug)
+    gi = get_object_or_404(GalleryImage, gallery=gallery, image=image)
+    return render_to_response(
+        "gallery_image.html",
+        dict(gallery=gallery,
+             image=image,
+             gi=gi))
 
-def image(request,slug):
-    image = get_object_or_404(Image,slug=slug)
-    return render_to_response("image.html",dict(image=image))
 
-def image_sets(request,slug,gallery_slug=None):
-    image = get_object_or_404(Image,slug=slug)
+def image(request, slug):
+    image = get_object_or_404(Image, slug=slug)
+    return render_to_response("image.html", dict(image=image))
+
+
+def image_sets(request, slug, gallery_slug=None):
+    image = get_object_or_404(Image, slug=slug)
     galleries = Gallery.objects.all()
     gallery = None
     gi = None
     next_image = None
     if gallery_slug is not None:
-        gallery = get_object_or_404(Gallery,slug=gallery_slug)
-        gi = get_object_or_404(GalleryImage,gallery=gallery,image=image)
+        gallery = get_object_or_404(Gallery, slug=gallery_slug)
+        gi = get_object_or_404(GalleryImage, gallery=gallery, image=image)
         next_image = gi.next_image()
     if request.method == "POST":
         image_galleries = [gi.gallery for gi in image.galleryimage_set.all()]
-        post_galleries = [get_object_or_404(Gallery,id=key[len("gallery_"):]) for key in request.POST.keys() if key.startswith('gallery_')]
+        post_galleries = [get_object_or_404(Gallery, id=key[len("gallery_"):])
+                          for key in request.POST.keys()
+                          if key.startswith('gallery_')]
         # remove any that need to be removed
         for g in image_galleries:
             if g not in post_galleries:
@@ -76,44 +93,50 @@ def image_sets(request,slug,gallery_slug=None):
             image.add_to_gallery(g)
 
         if request.POST['submit'] == "update":
-            return HttpResponseRedirect(request.META.get('HTTP_REFERER',image.get_absolute_url() + "sets/"))
+            return HttpResponseRedirect(
+                request.META.get('HTTP_REFERER',
+                                 image.get_absolute_url() + "sets/"))
         else:
             # save and next
-            if gallery == None:
+            if gallery is None:
                 # next image by date
                 next_image = image.next_image()
-            return HttpResponseRedirect(next_image.get_absolute_url() + "sets/")
+            return HttpResponseRedirect(
+                next_image.get_absolute_url() + "sets/")
 
     else:
         gdata = []
         for g in galleries:
             image_in = g.has_image(image)
-            gdata.append(dict(image_in=image_in,gallery=g))
+            gdata.append(dict(image_in=image_in, gallery=g))
 
-        return render_to_response("image_sets.html",dict(image=image,galleries=gdata))
+        return render_to_response("image_sets.html",
+                                  dict(image=image, galleries=gdata))
 
 
 @login_required
 def add_image(request):
     if request.method == "POST":
-        if request.POST.get("slug","") == "":
+        if request.POST.get("slug", "") == "":
             request.POST['slug'] = slugify(request.POST.get("title"))
-        form = AddImageForm(request.POST,request.FILES)
+        form = AddImageForm(request.POST, request.FILES)
         if form.is_valid():
             img = form.save(commit=False)
-            if request.FILES.get('image',None):
+            if request.FILES.get('image', None):
                 original_filename = request.FILES['image'].name
                 extension = os.path.splitext(original_filename)[1].lower()
                 if extension == ".jpeg":
                     extension = ".jpg"
-                if extension not in [".jpg",".png",".gif"]:
+                if extension not in [".jpg", ".png", ".gif"]:
                     return HttpResponse("unsupported image format")
                 register_openers()
                 datagen, headers = multipart_encode((
-                        ("t","upload"),
-                        MultipartParam(name='image',fileobj=request.FILES['image'],
-                                       filename="image%s" % extension)))
-                req = urllib2.Request("http://reticulum.thraxil.org/", datagen, headers)
+                        ("t", "upload"),
+                        MultipartParam(
+                            name='image', fileobj=request.FILES['image'],
+                            filename="image%s" % extension)))
+                req = urllib2.Request("http://reticulum.thraxil.org/",
+                                      datagen, headers)
                 metadata = loads(urllib2.urlopen(req).read())
                 img.ahash = metadata["hash"]
                 img.extension = extension
@@ -121,18 +144,18 @@ def add_image(request):
             img.save()
             for key in request.POST.keys():
                 if key.startswith("gallery_"):
-                    g = get_object_or_404(Gallery,id=key[len("gallery_"):])
+                    g = get_object_or_404(Gallery, id=key[len("gallery_"):])
                     img.add_to_gallery(g)
 
             return HttpResponseRedirect(img.get_absolute_url())
         else:
             print "not valid"
             galleries = Gallery.objects.all()
-            return render_to_response("add_image.html",dict(galleries=galleries,
-                                                            form=form))
+            return render_to_response("add_image.html",
+                                      dict(galleries=galleries,
+                                           form=form))
     else:
         galleries = Gallery.objects.all()
-        return render_to_response("add_image.html",dict(galleries=galleries,
-                                                    form=AddImageForm()))
-
-
+        return render_to_response("add_image.html",
+                                  dict(galleries=galleries,
+                                       form=AddImageForm()))
