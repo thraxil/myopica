bd515c755095f6e88e5d5414d0732270d63bbf43
Anders Pearson <anders@columbia.edu>
2013-07-03 22:47:05 -0400

    fabric deployment

---
+++ fabfile.py
@@ -0,0 +1,16 @@
+from fabric.api import run, sudo, local, cd, env
+
+env.hosts = ['oolong.thraxil.org', 'maru.thraxil.org']
+
+def restart_gunicorn():
+    sudo("restart myopica")
+
+def prepare_deploy():
+    local("./manage.py test")
+
+def deploy():
+    code_dir = "/var/www/myopica/myopica"
+    with cd(code_dir):
+        run("git pull origin master")
+        run("./bootstrap.py")
+    restart_gunicorn()
