e480df6a02e6420cc9fb181dc4af1dd8c4156a87
Anders Pearson <anders@columbia.edu>
2015-02-28 23:22:28 +0100

    cloudfront config needs the right variable set

--- myopica/settings_production.py
+++ myopica/settings_production.py
@@ -6,7 +6,7 @@ TEMPLATE_DIRS = (
     os.path.join(os.path.dirname(__file__), "templates"),
 )
 
-DEBUG = False
+DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 STATIC_ROOT = os.path.join(os.path.dirname(__file__), "../media")
@@ -14,18 +14,18 @@ STATICFILES_DIRS = ()
 COMPRESS_ENABLED = True
 COMPRESS_OFFLINE = True
 
+AWS_S3_CUSTOM_DOMAIN = "d3bk6m0iptukrc.cloudfront.net"
 AWS_STORAGE_BUCKET_NAME = "thraxil-myopica-static-prod"
 AWS_PRELOAD_METADATA = True
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-STATICFILES_STORAGE = 'cacheds3storage.CompressorS3BotoStorage'
-CLOUDFRONT_DOMAIN = "d3bk6m0iptukrc.cloudfront.net"
-S3_URL = 'https://%s/' % CLOUDFRONT_DOMAIN
-STATIC_URL = 'https://%s/media/' % CLOUDFRONT_DOMAIN
+STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+S3_URL = 'https://%s/' % AWS_S3_CUSTOM_DOMAIN
+STATIC_URL = 'https://%s/media/' % AWS_S3_CUSTOM_DOMAIN
 COMPRESS_ROOT = STATIC_ROOT
 COMPRESS_URL = STATIC_URL
 DEFAULT_FILE_STORAGE = 'cacheds3storage.MediaRootS3BotoStorage'
 MEDIA_URL = S3_URL + '/media/'
-COMPRESS_STORAGE = 'cacheds3storage.CompressorS3BotoStorage'
+COMPRESS_STORAGE = 'cacheds3storage.MediaRootS3BotoStorage'
 AWS_QUERYSTRING_AUTH = False
 
 
