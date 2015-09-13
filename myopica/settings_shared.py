6ef978bb8063261a029317e09f9cbf1b4aa24d5d
Anders Pearson <anders@columbia.edu>
2015-09-13 16:43:35 +0200

    convert to thraxilsettings

--- myopica/settings_shared.py
+++ myopica/settings_shared.py
@@ -1,126 +1,15 @@
 # Django settings for myopica project.
-import sys
 import os.path
+from thraxilsettings.shared import common
 
-DEBUG = True
-TEMPLATE_DEBUG = DEBUG
+app = 'myopica'
+base = os.path.dirname(__file__)
 
-ADMINS = (
-    ('Anders Pearson', 'anders@columbia.edu'),
-)
+locals().update(common(app=app, base=base))
 
-MANAGERS = ADMINS
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'myopica',
-        'HOST': '',
-        'PORT': 5432,
-        'USER': '',
-        'PASSWORD': '',
-    }
-}
-
-if 'test' in sys.argv:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': ':memory:',
-            'HOST': '',
-            'PORT': '',
-            'USER': '',
-            'PASSWORD': '', }}
-
-TIME_ZONE = 'America/New_York'
-LANGUAGE_CODE = 'en-us'
-
-SITE_ID = 1
-USE_I18N = False
-
-MEDIA_ROOT = '/var/tmp/myopica/uploads/'
-MEDIA_URL = '/uploads/'
-ADMIN_MEDIA_PREFIX = '/media/'
-SECRET_KEY = 'daod0^($fi6%pcy8(ihsj8$e0!&4@+=s8())redma#z9)v9d8*'
-
-TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
-)
-
-TEMPLATE_CONTEXT_PROCESSORS = (
-    'django.contrib.auth.context_processors.auth',
-    'django.core.context_processors.debug',
-    'django.core.context_processors.request',
-    'django.core.context_processors.static',
-)
-
-MIDDLEWARE_CLASSES = (
-    'django_statsd.middleware.GraphiteRequestTimingMiddleware',
-    'django_statsd.middleware.GraphiteMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
-)
-
-ROOT_URLCONF = 'myopica.urls'
-
-TEMPLATE_DIRS = (
-    "/home/anders/code/python/myopica/myopica/templates/",
-)
-
-INSTALLED_APPS = [
+INSTALLED_APPS += [  # noqa
     'myopica.portfolio',
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.sites',
-    'django.contrib.flatpages',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
     'django.contrib.sitemaps',
-    'django_statsd',
-    'django_markwhat',
-    'gunicorn',
-    'compressor',
-    'storages',
-    'smoketest',
 ]
 
-STATIC_URL = '/media/'
-STATICFILES_DIRS = (
-    os.path.abspath(os.path.join(os.path.dirname(__file__), "../media/")),
-)
-STATIC_ROOT = "/tmp/myopica/static"
-STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-    'compressor.finders.CompressorFinder',
-)
-COMPRESS_URL = "/media/"
-COMPRESS_ROOT = "media/"
-
-COMPRESS_CSS_FILTERS = [
-    'compressor.filters.css_default.CssAbsoluteFilter',
-    'compressor.filters.cssmin.CSSMinFilter',
-]
-
-THUMBNAIL_SUBDIR = "thumbs"
-TEST_RUNNER = 'django.test.runner.DiscoverRunner'
-
-SESSION_ENGINE = "django.contrib.sessions.backends.signed_cookies"
-SESSION_COOKIE_HTTPONLY = True
-
-STATSD_CLIENT = 'statsd.client'
-STATSD_PREFIX = 'myopica'
-STATSD_HOST = '127.0.0.1'
-STATSD_PORT = 8125
-
-ALLOWED_HOSTS = ['.localhost', 'myopica.org']
-
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': True,
-}
+ALLOWED_HOSTS += ['myopica.org']  # noqa
