967bd61d08722045e1a137ece58121a5de2c0456
Anders Pearson <anders@columbia.edu>
2016-08-09 20:17:01 +0100

    new style urls.py

--- myopica/urls.py
+++ myopica/urls.py
@@ -1,4 +1,8 @@
-from django.conf.urls import patterns, include
+import django.contrib.sitemaps.views
+import django.contrib.auth.views
+import django.views.static
+
+from django.conf.urls import include, url
 from django.contrib import admin
 from django.contrib.sitemaps import GenericSitemap
 from django.views.generic.detail import DetailView
@@ -23,27 +27,24 @@ sitemaps = {
     'images': GenericSitemap(image_info_dict, priority=0.6),
 }
 
-urlpatterns = patterns(
-    '',
-    (r'^$', views.IndexView.as_view()),
-    (r'^feeds/(?P<url>.*)/$', MainFeed()),
-    (r'^admin/', include(admin.site.urls)),
-    (r'^sitemap\.xml$',
-     'django.contrib.sitemaps.views.sitemap',
-     {'sitemaps': sitemaps}),
-    (r'^accounts/login/$', 'django.contrib.auth.views.login'),
-    (r'smoketest/', include('smoketest.urls')),
-    (r'^uploads/(?P<path>.*)$',
-     'django.views.static.serve',
-     {'document_root': '/var/tmp/myopica/media/'}),
-    (r'^image/(?P<slug>[^/]+)/$', DetailView.as_view(model=Image)),
-    (r'^image/(?P<slug>[^/]+)/sets/$', views.ImageSetsView.as_view()),
-    (r'^add_image/$', views.AddImageView.as_view()),
-    (r'^(?P<slug>[^/]+)/$', DetailView.as_view(model=Gallery)),
-    (r'^(?P<slug>[^/]+)/reorder/$',
-     views.ReorderGalleryView.as_view()),
-    (r'^(?P<gallery_slug>[^/]+)/(?P<image_slug>[^/]+)/$',
-     views.GalleryImageView.as_view()),
-    (r'^(?P<gallery_slug>[^/]+)/(?P<slug>[^/]+)/sets/$',
-     views.ImageSetsView.as_view()),
-)
+urlpatterns = [
+    url(r'^$', views.IndexView.as_view()),
+    url(r'^feeds/(?P<url>.*)/$', MainFeed()),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^sitemap\.xml$', django.contrib.sitemaps.views.sitemap,
+        {'sitemaps': sitemaps}),
+    url(r'^accounts/login/$', django.contrib.auth.views.login),
+    url(r'smoketest/', include('smoketest.urls')),
+    url(r'^uploads/(?P<path>.*)$', django.views.static.serve,
+        {'document_root': '/var/tmp/myopica/media/'}),
+    url(r'^image/(?P<slug>[^/]+)/$', DetailView.as_view(model=Image)),
+    url(r'^image/(?P<slug>[^/]+)/sets/$', views.ImageSetsView.as_view()),
+    url(r'^add_image/$', views.AddImageView.as_view()),
+    url(r'^(?P<slug>[^/]+)/$', DetailView.as_view(model=Gallery)),
+    url(r'^(?P<slug>[^/]+)/reorder/$',
+        views.ReorderGalleryView.as_view()),
+    url(r'^(?P<gallery_slug>[^/]+)/(?P<image_slug>[^/]+)/$',
+        views.GalleryImageView.as_view()),
+    url(r'^(?P<gallery_slug>[^/]+)/(?P<slug>[^/]+)/sets/$',
+        views.ImageSetsView.as_view()),
+]
