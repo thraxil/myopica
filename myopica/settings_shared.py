ea32b7a134af05e00c3c02498d904265df2b77c1
Anders Pearson <anders@columbia.edu>
2016-08-21 17:54:06 +0100

    :arrow_up: Django 1.10

--- myopica/settings_shared.py
+++ myopica/settings_shared.py
@@ -7,6 +7,27 @@ base = os.path.dirname(__file__)
 
 locals().update(common(app=app, base=base))
 
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
+            os.path.join(os.path.dirname(__file__), "templates"),
+        ],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.contrib.auth.context_processors.auth',
+                'django.template.context_processors.debug',
+                'django.template.context_processors.media',
+                'django.template.context_processors.static',
+                'django.template.context_processors.tz',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+
 INSTALLED_APPS += [  # noqa
     'myopica.portfolio',
     'django.contrib.sitemaps',
