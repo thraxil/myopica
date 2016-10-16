5c2737e721a316df56c9ce85850c5643ea067132
Anders Pearson <anders@columbia.edu>
2016-10-16 19:17:36 +0100

    switch to single stage docker build

--- config.mk
+++ config.mk
@@ -10,14 +10,8 @@ REQUIREMENTS ?= requirements.txt
 VIRTUALENV ?= virtualenv.py
 SUPPORT_DIR ?= requirements/virtualenv_support/
 WHEELHOUSE ?= wheelhouse
-
 JS_FILES ?= media/js/
-
-ifeq ($(TAG), undefined)
-	IMAGE ?= $(REPO)/$(APP)
-else
-	IMAGE ?= $(REPO)/$(APP):$(TAG)
-endif
-
+TAG ?= latest
+IMAGE ?= $(REPO)/$(APP):$(TAG)
 MAX_COMPLEXITY ?= 10
 
